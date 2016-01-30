e9e002bcea9c935a06c7257a4a0352d469f0b549
Ivan Severin <ivan.severin.m@gmail.com>
2016-01-31 00:57:45 +0200

    Adding some prject files

---
+++ Output Ardiuno/Test_serial_output.ino
@@ -0,0 +1,12 @@
+#include <Mouse.h>
+
+void setup() {
+  // serial port initialization
+ // Serial.begin(9600);
+}
+
+void loop() {
+  // Printing 
+  //Serial.println("Hello Masha! I'm Arduino Nano!");
+  //delay(1000);
+}
