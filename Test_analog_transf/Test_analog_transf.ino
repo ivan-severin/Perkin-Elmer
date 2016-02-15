ecb595054cb94e780f3a670107c8c4f0e1804273
Ivan Severin <ivan.severin.m@gmail.com>
2016-02-15 09:19:07 +0200

    was goten some fixes

--- Test_analog_transf/Test_analog_transf.ino
+++ Test_analog_transf/Test_analog_transf.ino
@@ -1,21 +1,82 @@
-const int WNM = 2;
-const int scan = 3;
-const int pen = A0;
-const int T = 10; // period of reading data
-int wLen = 1;
-float wNum = 4000.0;
+const int WNM = 2;  // pin of wavenumver
+const int scan = 3; // pin of pauses in mesuring 
+const int pen = A0; // pin of analog intensyvity
+const int T = 1000; // period of reading data
+int wLen = 1;       // lengh of periodic peaks
+float wNum = 3800.0;
 float dt = 0.0;
+
+
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(wNum, INPUT);
   pinMode(scan, INPUT);
   Serial.println("Setup started");
-  //wLen = timeStep();
-  wLen = 1199;
+  wLen = timeStep();
+  //wLen = 1199;
   dt = 100.0 / float(wLen);
 }
 
 void loop() {
+
+  if (Serial.available() > 0  ){
+    cmd = Serial.read();
+  }
+  case 
+  read_data();
+  delay(T);
+
+}
+
+float timeStep() {
+
+  unsigned long int t0 = 0;
+  unsigned long int t1 = 0;
+  unsigned long int t2 = 0;
+  bool xOld = HIGH;
+  bool x = HIGH;            // get signal (periodic low)
+  bool k = 0;               // defines start and end of measuring time step
+  while (1) {
+    x = digitalRead(WNM);
+    if (x == LOW)                // 
+      Serial.println("low");
+    else
+      Serial.println("high");
+    if (x == LOW && xOld == HIGH && k == 0) {
+      t0 = millis(); 
+      Serial.print("t0 =  ");
+      Serial.println(t0);
+      k = 1;
+    }
+    else if (x == HIGH && xOld == LOW && k == 1)  {
+      t1 = millis(); 
+      Serial.print("t1 =  ");
+      Serial.println(t1);
+    }
+    else if (x == LOW && xOld == HIGH && k == 1)  {
+      t2 = millis();
+      k = 0;
+      Serial.print("t2 =  ");
+      Serial.println(t2);
+      break;
+    }
+
+    delay(T);
+
+    xOld = x;
+  }
+  Serial.println("breaked");
+  int peak = t1 - t0;
+  int wLen = t2 - t0;
+  //wLen = 500;
+  Serial.println(float(wLen)/float(T));
+  return float(wLen)/float(T);
+}
+
+
+void read_data(){
   if (digitalRead(scan) == LOW && wNum > 200 ) {
 
     Serial.print(wNum);
@@ -23,51 +84,17 @@ void loop() {
     Serial.println(float(analogRead(pen)) / 10.24);
     wNum -= dt;
 
-    
+
   }
-  else {Serial.println("scan OFF");}
+  else {
+    Serial.println("scan OFF");
+  }
 
 
-    delay(T);
 
 }
 
-int timeStep() {
-  int t = 0;
-  int t0 = 0;
-  int t1 = 0;
-  int t2 = 0;
-  int xOld = HIGH;
-  int x = HIGH;
-  bool k = 0;// defines start and end of measuring time step
-  while (1) {
-    x = digitalRead(WNM);
-    if(x==LOW) Serial.println("low");else Serial.println("high");
-    if (x == LOW && xOld == HIGH && k == 0) {
-      t0 = t;Serial.println("t0 seted");
-      k = 1;
-    }
-    else if (x == HIGH && xOld == LOW && k == 1)  {
-      t1 = t;Serial.println("t1 seted");
-    }
-    else if (x == LOW && xOld == HIGH && k == 1)  {
-      t2 = t;
-      k = 0;
-      Serial.println("t2 seted");
-      break;
-    }
-    
-    delay(T);
-    t += 1;
-    xOld = x;
-  }
-  Serial.println("breaked");
-  int peak = t1 - t0;
-  int wLen = t2 - t0;
-  //wLen = 500;
-  Serial.println(wLen);
-  return wLen;
-}
+
 
 
 
