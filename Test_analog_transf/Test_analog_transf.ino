ed6af04809d8fe04deaf93d449a2392aa3d1ed50
ivan-severin <ivan.severin.m@gmail.com>
2016-08-06 14:17:44 +0300

    Modefited Arduino script

--- Test_analog_transf/Test_analog_transf.ino
+++ Test_analog_transf/Test_analog_transf.ino
@@ -1,12 +1,12 @@
 const int WNM = 2;  // pin of wavenumver
-const int scan = 3; // pin of pauses in mesuring 
+const int scan = 3; // pin of pauses in mesuring
 const int pen = A0; // pin of analog intensyvity
-const int T = 1000; // period of reading data
+const int T = 50; // period of reading data
 int wLen = 1;       // lengh of periodic peaks
 float wNum = 3800.0;
 float dt = 0.0;
 
-// Settings
+////////////////////////////  Settings //////////////////////////////////////////////
 int measure;
 int channel;
 unsigned short scanTtime = 6;
@@ -19,30 +19,88 @@ bool abs_ = 0;
 
 
 
-
+//////////////////////////// Setup //////////////////////////////////////////////////
 void setup() {
   Serial.begin(9600);
   pinMode(wNum, INPUT);
   pinMode(scan, INPUT);
   Serial.println("Setup started");
- // wLen = timeStep();
+  //wLen = timeStep();
   //wLen = 1199;
-  dt = 100.0 / float(wLen);
+  //dt = 100.0 / float(wLen);
 }
-/////////////////////////////////////////////////////////////////////
+///////////////////////////  Main  Loop //////////////////////////////////////////
 void loop() {
 
   //if (Serial.available() > 0  ){
-    //bool cmd = Serial.read();
+  //bool cmd = Serial.read();
   //}
 
-  //read_data();
-  send_data();
-  dt+=1.0;
+  read_data();
+  //send_data();
+  //dt += 1.0;
   delay(T);
 
 }
-////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+void read_data() {
+  if (digitalRead(scan) == LOW  ) {
+
+    Serial.print(dt);
+    Serial.print('\t');
+    Serial.println(analogRead(pen));
+    dt += 1.0;
+    }
+  else {
+    //Serial.println("scan OFF");
+  }
+}
+
+bool getStatus() {
+
+
+}
+
+
+bool getSettings() {
+  int incomingByte = 0;
+  if (Serial.available() > 0) {
+
+    delay(10);
+
+    //for ( int i = 0; i < Serial.available(); i++ )  {   /// check this num
+
+    if (Serial.read() == 0xAC) {
+
+      incomingByte = Serial.read();
+
+      switch (incomingByte) {
+        case 0x01:
+          measure = true;
+          channel = 0;
+          break;
+
+        case 0x02:
+          measure = true;
+          channel = 0;
+          break;
+      }
+    }
+  }
+
+  return 0;
+}
+
+void send_data() {
+
+  Serial.print(dt);
+  Serial.print('\t');
+  Serial.println(float(analogRead(pen)) / 10.23);
+
+
+}
+
+
 float timeStep() {
 
   unsigned long int t0 = 0;
@@ -53,18 +111,18 @@ float timeStep() {
   bool k = 0;               // defines start and end of measuring time step
   while (1) {
     x = digitalRead(WNM);
-    if (x == LOW)                // 
+    if (x == LOW)                //
       Serial.println("low");
     else
       Serial.println("high");
     if (x == LOW && xOld == HIGH && k == 0) {
-      t0 = millis(); 
+      t0 = millis();
       Serial.print("t0 =  ");
       Serial.println(t0);
       k = 1;
     }
     else if (x == HIGH && xOld == LOW && k == 1)  {
-      t1 = millis(); 
+      t1 = millis();
       Serial.print("t1 =  ");
       Serial.println(t1);
     }
@@ -84,88 +142,9 @@ float timeStep() {
   int peak = t1 - t0;
   int wLen = t2 - t0;
   //wLen = 500;
-  Serial.println(float(wLen)/float(T));
-  return float(wLen)/float(T);
+  Serial.println(float(wLen) / float(T));
+  return float(wLen) / float(T);
 }
-
-///////////////////////////////////////////////////////////////////////
-void read_data(){
-  if (digitalRead(scan) == LOW && wNum > 200 ) {
-
-    Serial.print(wNum);
-    Serial.print('\t');
-    Serial.println(float(analogRead(pen)) / 10.24);
-    wNum -= dt;
-
-
-  }
-  else {
-    Serial.println("scan OFF");
-  }
-
-
-
-}
-
-bool getStatus(){
-
-
-}
-
-
-bool getSettings() {
-  int incomingByte = 0;
-  if (Serial.available() > 0) {
-
-    delay(10);
-
-    //for ( int i = 0; i < Serial.available(); i++ )  {   /// check this num
-
-    if(Serial.read()==0xAC) {
-
-      incomingByte = Serial.read();
-
-      switch (incomingByte) {
-
-
-
-      case 0x01:
-
-        measure=true;
-
-        channel=0;
-
-        break;
-        
-      case 0x02:
-
-        measure=true;
-
-        channel=0;
-
-        break;
-      }
-
-
-    }
-
-
-
-
-  }
-
-  return 0;
-}
-
-void send_data(){
-  
-  Serial.print(dt);
-  Serial.print('\t');
-  Serial.println(float(analogRead(pen)) / 10.24);
-  
-  
-}
-
 
 
 
