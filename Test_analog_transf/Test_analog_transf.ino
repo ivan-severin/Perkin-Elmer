a7041ab18cbe8efcab942f86de766f927d8d99be
Ivan Severin <Ivan.severin.m@gmail.com>
2016-09-02 20:58:39 +0300

    almost Fixed problem with interruptts

--- Test_analog_transf/Test_analog_transf.ino
+++ Test_analog_transf/Test_analog_transf.ino
@@ -7,37 +7,43 @@
 #define start_pin 5
 
 
-volatile int wNum = 0;     // wavenumver
-volatile int pen = 0;         // relate  Transmiteence
+volatile float wNum = 4000.0;     // wavenumver
+volatile float pen = 0.0;         // relate  Transmiteence
 float pen_old = 0.0;
- QueueArray <int> pen_queue;
- QueueArray <int> wnum_queue;
+QueueArray <float> pen_queue;
+QueueArray <float> wnum_queue;
+bool scan = false;
 bool s = false;
-
 
 //////////////////////////// Setup //////////////////////////////////////////////////
 void setup() {
   Serial.begin(38400);
   // set the printer of the queue.
-    pen_queue.setPrinter (Serial);
-   wnum_queue.setPrinter (Serial);
-  attachInterrupt(0, read_data, FALLING);
- // pinMode(WNm_pin, INPUT);
-  pinMode(scan_pin, INPUT);
+  pen_queue.setPrinter (Serial);
+  wnum_queue.setPrinter (Serial);
+  //attachInterrupt(0, read_data, RISING);
+  pinMode(WNm_pin, INPUT_PULLUP);
+  //pinMode(scan_pin, INPUT);
 
   Serial.println("Setup started");
 }
 ///////////////////////////  Main  Loop //////////////////////////////////////////
 void loop() {
+  s = !digitalRead(scan_pin);
+  if (s ){
+    if (! scan){ 
+      attachInterrupt(0, read_data, FALLING);
+      Serial.println("attachInterrupt");
+    }
+    scan = true;
+    
+  }
+  else {
+    detachInterrupt(0);
+    Serial.println("detachInterrupt");
+  }
 
-//  if (! digitalRead(scan_pin)){
-//    attachInterrupt(0, read_data, FALLING);
-//  }
-//  else {
-//    detachInterrupt(0);
-//  }
-
-  while(!pen_queue.isEmpty () || !wnum_queue.isEmpty ()){
+  if( !pen_queue.isEmpty() ){
     Serial.print(wnum_queue.pop());
     Serial.print('\t');
     Serial.println(pen_queue.pop());
@@ -49,11 +55,12 @@ void loop() {
 
 ////////////////////////// Read data ////////////////////////////////////////////
 void read_data() {
-  wNum++;
+  wNum -= 0.1;
   pen=analogRead(pen_pin);
   pen_queue.push(pen);
   wnum_queue.push(wNum);
 }
+
 
 
 
